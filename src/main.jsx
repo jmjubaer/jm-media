a21aa9db2e99cf8e05e6ece9f890bc908a76ed3a
jmjubaer <jmjubaer3927@gmail.com>
2023-07-17 12:03:33 +0600

    basic setup

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,19 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
 import './index.css'
+import { RouterProvider } from 'react-router-dom'
+import routes from './Routes/routes.jsx'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import AuthProvider from './Providers/AuthProvider'
+
+const queryClient = new QueryClient;
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <QueryClientProvider client={queryClient}>
+        <AuthProvider>
+            <RouterProvider router={routes}/>
+        </AuthProvider>
+    </QueryClientProvider>
   </React.StrictMode>,
 )
